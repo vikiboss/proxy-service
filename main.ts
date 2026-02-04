c596f1c6bfc13dfdbdead7270f5add4b8758df7a
Viki <hi@viki.moe>
2026-02-04 10:46:26 +0800

    feat: 支持 proxy-referrer 配置项

--- main.ts
+++ main.ts
@@ -1,47 +1,51 @@
-const md2htmlApi = "https://md2html.viki.moe";
-const markdownContent = await Deno.readTextFile("./README.md");
+const md2htmlApi = 'https://md2html.viki.moe'
+const markdownContent = await Deno.readTextFile('./README.md')
 
 Deno.serve((request: Request) => {
-	const url = new URL(request.url);
-	const [sp, reqHeaders] = [url.searchParams, request.headers];
+  const url = new URL(request.url)
+  const [sp, reqHeaders] = [url.searchParams, request.headers]
 
-	const [hostname, port, protocol] = [
-		sp.get("proxy-host") || reqHeaders.get("proxy-host"),
-		sp.get("proxy-port") || reqHeaders.get("proxy-port"),
-		sp.get("proxy-protocol") || reqHeaders.get("proxy-protocol"),
-	];
+  const [hostname, port, protocol, referrer] = [
+    sp.get('proxy-host') || reqHeaders.get('proxy-host'),
+    sp.get('proxy-port') || reqHeaders.get('proxy-port'),
+    sp.get('proxy-protocol') || reqHeaders.get('proxy-protocol'),
+    sp.get('proxy-referrer') || reqHeaders.get('proxy-referrer'),
+  ]
 
-	if (!hostname) {
-		if (url.pathname === '/favicon.ico') {
-			return fetch('https://avatar.viki.moe');
-		}
-		return fetch(md2htmlApi, { method: "POST", body: markdownContent });
-		
-	}
+  if (!hostname) {
+    if (url.pathname === '/favicon.ico') {
+      return fetch('https://avatar.viki.moe')
+    }
+    return fetch(md2htmlApi, { method: 'POST', body: markdownContent })
+  }
 
-	url.hostname = hostname || "viki.moe";
-	url.port = port || "443";
-	url.protocol = protocol || "https:";
+  url.hostname = hostname || 'viki.moe'
+  url.port = port || '443'
+  url.protocol = protocol || 'https:'
 
-	sp.delete("proxy-host");
-	sp.delete("proxy-port");
-	sp.delete("proxy-protocol");
+  sp.delete('proxy-host')
+  sp.delete('proxy-port')
+  sp.delete('proxy-protocol')
+  sp.delete('proxy-referrer')
 
-	const headers = new Headers(reqHeaders);
+  const headers = new Headers(reqHeaders)
 
-	headers.delete("proxy-host");
-	headers.delete("proxy-port");
-	headers.delete("proxy-protocol");
+  headers.delete('proxy-host')
+  headers.delete('proxy-port')
+  headers.delete('proxy-protocol')
+  headers.delete('proxy-referrer')
 
-	headers.set("Access-Control-Allow-Origin", "*");
-	headers.set(
-		"Access-Control-Allow-Methods",
-		"GET, POST, PUT, DELETE, OPTIONS",
-	);
+  if (referrer) {
+    headers.set('referer', referrer)
+  }
 
-	return fetch(url.href, {
-		headers,
-		method: request.method,
-		body: request.body,
-	});
-});
+  headers.set('Access-Control-Allow-Origin', '*')
+  headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+
+  return fetch(url.href, {
+    headers,
+    method: request.method,
+    body: request.body,
+    redirect: 'follow',
+  })
+})
